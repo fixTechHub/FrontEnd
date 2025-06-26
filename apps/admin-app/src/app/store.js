1a42da3bc22155ea2b180e534dc70f44f3f768d1
129171 <135350963+129171@users.noreply.github.com>
2025-06-26 16:55:10 +0700

    add view deposit log add deposit in technician in admin-apps

---
+++ apps/admin-app/src/app/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+import technicianReducer from '../features/technician/technicianSlice';
+import transactionReducer from '../features/technician/technicianSlice';
+import transactionReducer from '../features/transactions/transactionSlice'
+
+export const store = configureStore({
+    reducer: {
+        technician: technicianReducer,
+        transaction: transactionReducer
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
+
+export default store;
