f0cd5a8f0464cc47dbd7c36c23211001896f9ba5
dxtuptown <mailcuakhanhdat@gmail.com>
2025-08-31 17:40:00 +0700

    #ChangeCurrency

--- apps/admin-app/src/pages/home/admin-dashboard.jsx
+++ apps/admin-app/src/pages/home/admin-dashboard.jsx
@@ -220,7 +220,8 @@ const AdminDashboard = () => {
           color: '#6B7280',
           padding: 8,
           callback: function(value) {
-            return value >= 1000 ? (value/1000).toFixed(1) + 'K' : value.toLocaleString('en-US');
+            // return value >= 1000 ? (value/1000).toFixed(1) + 'K' : value.toLocaleString('en-US');
+            return value.toLocaleString('en-US') + ' VND'                                                                                       ;
           }
         },
         border: {
